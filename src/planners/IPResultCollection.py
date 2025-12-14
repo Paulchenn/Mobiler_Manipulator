7dc3c668a0019f397f547ea5cd05d19d7f59de94
Paulchenn <154928432+Paulchenn@users.noreply.github.com>
2025-12-14 17:30:04 +0100

    Add ResultCollection class for storing planner results and performance data

---
+++ src/planners/IPResultCollection.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""
+This code is part of the course "Introduction to robot path planning" (Author: Bjoern Hein).
+
+License is based on Creative Commons: Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) (pls. check: http://creativecommons.org/licenses/by-nc/4.0/)
+"""
+
+class ResultCollection (object):
+    def __init__(self, plannerFactoryName, planner, benchmark, solution, perfDataFrame):
+        self.plannerFactoryName = plannerFactoryName
+        self.planner = planner
+        self.benchmark = benchmark
+        self.solution = solution
+        self.perfDataFrame = perfDataFrame
