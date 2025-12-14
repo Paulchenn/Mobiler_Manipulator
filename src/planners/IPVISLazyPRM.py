bc9fbceadcf831ec651e0b01bc71561993ef014c
Paulchenn <154928432+Paulchenn@users.noreply.github.com>
2025-12-14 17:30:33 +0100

    Refactor lazyPRMVisualize function to use 2D positions for drawing nodes and edges

--- src/planners/IPVISLazyPRM.py
+++ src/planners/IPVISLazyPRM.py
@@ -16,6 +16,7 @@ def lazyPRMVisualize(planner, solution = [] , ax=None, nodeSize = 300):
     nonCollEdges = planner.nonCollidingEdges
     # get a list of positions of all nodes by returning the content of the attribute 'pos'
     pos = nx.get_node_attributes(graph,'pos')
+    pos_xy  = {k: v[:2] for k, v in pos.items()}
     color = nx.get_node_attributes(graph,'color')
 
     collChecker.drawObstacles(ax)
@@ -25,29 +26,24 @@ def lazyPRMVisualize(planner, solution = [] , ax=None, nodeSize = 300):
     #degree = nx.degree_centrality(graph)
     
     # draw graph
-    nx.draw_networkx_nodes(graph, pos, ax = ax, nodelist=list(color.keys()), node_color=list(color.values()), node_size=nodeSize)
-    nx.draw_networkx_edges(graph, pos, ax = ax)
+    nx.draw_networkx_nodes(graph, pos_xy, ax=ax, nodelist=list(color.keys()), node_color=list(color.values()), node_size=nodeSize)
+    nx.draw_networkx_edges(graph, pos_xy, ax=ax)
 
 
-    
     # draw all connected components, emphasize the largest one
     Gcc=(graph.subgraph(c) for c in nx.connected_components(graph))
     G0=next(Gcc) # [0] = largest connected component
     
     # how largest connected component
-    nx.draw_networkx_edges(G0,pos,
-                               edge_color='b',
-                               width=3.0, style='dashed',
-                               alpha=0.5,
-                            )
+    nx.draw_networkx_edges(G0, pos_xy, edge_color='b', width=3.0, style='dashed', alpha=0.5,)
     if collEdges != []:
         collGraph = nx.Graph()
         collGraph.add_nodes_from(graph.nodes(data=True))
 
         #collGraph
         for i in collEdges:
-            collGraph.add_edge(i[0],i[1])
-        nx.draw_networkx_edges(collGraph,pos,alpha=0.2,edge_color='r',width=5)
+            collGraph.add_edge(i[0], i[1])
+        nx.draw_networkx_edges(collGraph, pos_xy, alpha=0.2, edge_color='r', width=5)
 
 
     
@@ -57,32 +53,28 @@ def lazyPRMVisualize(planner, solution = [] , ax=None, nodeSize = 300):
 
         #collGraph
         for i in nonCollEdges:
-            nonCollGraph.add_edge(i[0],i[1])
-        nx.draw_networkx_edges(nonCollGraph,pos,alpha=0.8,edge_color='yellow',width=5)
+            nonCollGraph.add_edge(i[0], i[1])
+        nx.draw_networkx_edges(nonCollGraph, pos_xy, alpha=0.8, edge_color='yellow', width=5)
     
 
 
     # draw start and goal
     if "start" in graph.nodes(): 
-        nx.draw_networkx_nodes(graph,pos,nodelist=["start"],
-                                   node_size=300,
-                                   node_color='#00dd00',  ax = ax)
-        nx.draw_networkx_labels(graph,pos,labels={"start": "S"},  ax = ax)
+        nx.draw_networkx_nodes(graph, pos_xy, nodelist=["start"], node_size=300, node_color='#00dd00',  ax=ax)
+        nx.draw_networkx_labels(graph, pos_xy, labels={"start": "S"},  ax=ax)
 
 
     if "goal" in graph.nodes():
-        nx.draw_networkx_nodes(graph,pos,nodelist=["goal"],
-                                   node_size=300,
-                                   node_color='#DD0000',  ax = ax)
-        nx.draw_networkx_labels(graph,pos,labels={"goal": "G"},  ax = ax)
+        nx.draw_networkx_nodes(graph, pos_xy, nodelist=["goal"], node_size=300, node_color='#DD0000',  ax=ax)
+        nx.draw_networkx_labels(graph, pos_xy, labels={"goal": "G"},  ax=ax)
 
 
 
     if solution != []:
         # draw nodes based on solution path
-        Gsp = nx.subgraph(graph,solution)
+        Gsp = nx.subgraph(graph, solution)
         # draw edges based on solution path
-        nx.draw_networkx_edges(Gsp,pos,alpha=0.8,edge_color='g',width=10)
+        nx.draw_networkx_edges(Gsp, pos_xy, alpha=0.8, edge_color='g', width=10,  ax=ax)
     
 
     
